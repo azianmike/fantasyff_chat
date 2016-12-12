d97bfcb3f808c807a50d0b757051cf66dea1be22
Michael Luo <michaell@zillow.com>
2016-12-11 22:06:12 -0800

    Got live team scoring working. Currently stuck in recursive retry hell though

---
+++ getNFLData.js
@@ -0,0 +1,58 @@
+/**
+ * Created by michaell on 12/11/16.
+ */
+
+var http = require('http');
+
+/**
+ * Gets live NFL data from nfl.com and then performs a function on the JSON data
+ * @param functionToCall function to call after getting JSON data
+ */
+function getLiveNFLData(){
+    var options = {
+        host: 'www.nfl.com',
+        port: 80,
+        path: '/liveupdate/scorestrip/ss.json'
+    };
+
+    return options
+}
+
+function getTeamLiveScore( teamName )
+{
+    var req = http.get(getLiveNFLData(), function(resp){
+        var retry = false
+        resp.setEncoding('utf8');
+        if( resp.statusCode == 200 && !retry ) {
+            resp.on('data', function (chunk) {
+                // Parses the JSON data
+                if( !retry ) {
+                    try {
+                        var jsonResult = JSON.parse(chunk);
+                        var games = jsonResult.gms
+                        for (var game in games) {
+                            var gameObj = games[game]
+                            if (gameObj.vnn.toLowerCase() === teamName.toLowerCase() || gameObj.hnn.toLowerCase() === teamName.toLowerCase()) {
+                                console.log("Score is " + gameObj.vs + " to " + gameObj.hs)
+                                return;
+                            }
+                        }
+                        console.log("Not found")
+                        console.log(jsonResult)
+                    } catch (err) {
+                        retry = true;
+                        getTeamLiveScore(teamName)
+                    }
+                }
+
+            });
+        }
+    }).on("error", function(e){
+        console.log("Got error: " + e.message);
+    });
+
+    req.end()
+}
+
+getTeamLiveScore("Steelers")
+
