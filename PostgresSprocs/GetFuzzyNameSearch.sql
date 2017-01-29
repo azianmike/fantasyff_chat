37cb2196a9ece03f2612addc521aad5c23fe5de1
Michael Luo <michaell@zillow.com>
2017-01-28 16:47:48 -0800

    Updating get fuzzy name search sproc

--- PostgresSprocs/GetFuzzyNameSearch.sql
+++ PostgresSprocs/GetFuzzyNameSearch.sql
@@ -1,15 +1,12 @@
 CREATE OR REPLACE FUNCTION GetFuzzyNameSearch(name VARCHAR(70))
-RETURNS refcursor AS
+RETURNS TABLE (full_name VARCHAR, distance INT) as
 $BODY$
-DECLARE
-  ref refcursor;
 
 BEGIN
-        OPEN ref FOR SELECT full_name, levenshtein(full_name, name) as distance
+        RETURN QUERY SELECT player.full_name, levenshtein(player.full_name, name) as distance
                                  FROM player
-                                 WHERE levenshtein(full_name, name) IS NOT NULL
+                                 WHERE levenshtein(player.full_name, name) IS NOT NULL and levenshtein(player.full_name, name) < 6
                                  ORDER BY distance ASC LIMIT 3;
-RETURN ref;
 END;
 $BODY$
 LANGUAGE plpgsql STABLE
