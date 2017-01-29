36ba28c04fa7941f37e35817873203669bebd9b1
Ubuntu <ubuntu@ip-172-31-31-55.ec2.internal>
2017-01-29 00:47:24 +0000

    Updating test_postgres for result set

--- test_postgres.js
+++ test_postgres.js
@@ -3,6 +3,7 @@ var config =  require('./PostgresFunctions/PostgresConfig');
 //config.executePostgresQuery('select GetPlayerPassingSeasonStats(\'Tom Brady\', 2015);');
 function tempFunc(rows)
 {
+console.log("test");
 console.log(rows.rows);
 }
 config.executePostgresQuery('select GetFuzzyNameSearch(\'Tom Bardy\');', tempFunc);
