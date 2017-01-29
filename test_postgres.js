d57059959cc53e2ad7ba484a759e82453858625b
Ubuntu <ubuntu@ip-172-31-31-55.ec2.internal>
2017-01-29 00:33:25 +0000

    Adding new test_postgres testing

--- test_postgres.js
+++ test_postgres.js
@@ -1,3 +1,8 @@
 var config =  require('./PostgresFunctions/PostgresConfig');
 
-config.executePostgresQuery('select GetPlayerPassingSeasonStats(\'Tom Brady\', 2015);');
+//config.executePostgresQuery('select GetPlayerPassingSeasonStats(\'Tom Brady\', 2015);');
+function tempFunc(rows)
+{
+console.log(rows.rows);
+}
+config.executePostgresQuery('select GetFuzzyNameSearch(\'Tom Bardy\');', tempFunc);
