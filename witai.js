ec03744e73f044ad41272e4f5f102c426f583f40
Michael Luo <michaell@zillow.com>
2017-03-17 19:16:59 -0700

    Updated warn function

--- witai.js
+++ witai.js
@@ -169,7 +169,7 @@ function callActionHelper(context, callbackFunc, sender) {
     }
     else {
         context['errorMsg'] = "No context/bad context";
-        log.warning(context);
+        log.warn(context);
         callbackFunc("We\'re not sure what you meant. Type \'help\' for guidance from the bot!")
     }
 }
