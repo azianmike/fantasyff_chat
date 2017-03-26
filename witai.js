252836fb1781d0b64d30da5d71c37e9e9426b5ba
Michael Luo <michaell@zillow.com>
2017-03-25 17:30:12 -0700

    Adding player info help

--- witai.js
+++ witai.js
@@ -139,6 +139,10 @@ const actions = {
                 callbackFunc("You can ask for category leaders for any stat (yards, touchdowns, sacks, etc) during any given time period!");
                 callbackFunc("Try it out! Ask \'who was the top rusher in 2014 between week 4 and 10\' or \'who had the most sacks in week 10?\'");
             }
+            else if(helpEntity == "player_info_help") {  // Stats help
+                callbackFunc("You can ask for player info, like how tall a player is or what team they play for!");
+                callbackFunc("Try it out! Ask \'how tall is tom brady\' or \'how many years has cam newton played in the league\'");
+            }
             else {  // Score help
                 callbackFunc("You can ask for lots of different scores of different teams! If you don't specify a week/year, we'll give you the latest game (or live score!)");
                 callbackFunc("You can also ask for scores between teams, like \'score of the ravens steelers game\'");
@@ -160,9 +164,15 @@ const actions = {
                 "title":"Get Top Stats",
                 "payload":"top_stats_help"
             }
+            var getPlayerInfoHelp = {
+                "content_type":"text",
+                "title":"Getting Player Info",
+                "payload":"player_info_help"
+            }
             quick_replies.push(statsQuickReply);
             quick_replies.push(scoreQuickReply);
             quick_replies.push(getTopStatsReply);
+            quick_replies.push(getPlayerInfoHelp);
             fb_apicalls.sendQuickReplies(sender, "What do you need help with?", quick_replies);
         }
     },
