7d97796f7103d9363e92718fc19188018b1a2462
Michael Luo <michaell@zillow.com>
2017-09-24 16:17:00 -0700

    Update generate notifications to get whole game stats

--- PostgresSprocs/GenerateNotifications.sql
+++ PostgresSprocs/GenerateNotifications.sql
@@ -12,13 +12,7 @@ BEGIN
     SELECT MAX(lastProcessedPlayID) as lastProcessedPlayID, MAX(lastProcessedGsisID) as lastProcessedGsisID, player_id
     FROM playerSubscription GROUP BY player_id;
 
---    CREATE TEMP TABLE nullProcessedPlays AS
---    SELECT MAX(play_id) AS playID, MAX(gsis_id) AS gsisID
---    FROM play_player AS pp INNER JOIN playersToCheck
---    on pp.player_id = playersToCheck.player_id
---    GROUP BY pp.player_id;
-
-    -- Update the lastProcessedPlayID as the play right before the first play in the game (if exists)
+    -- Update the last processes GSIS and playID if null to latest play and game
     UPDATE playersToCheck as ptc
     SET lastProcessedGsisID = (SELECT MAX(gsis_id) FROM play_player as pp where ptc.player_id=pp.player_id),
     lastProcessedPlayID = (SELECT MAX(play_id) FROM play_player as pp where ptc.player_id=pp.player_id and gsis_id = (SELECT MAX(gsis_id) FROM play_player as pp where ptc.player_id=pp.player_id))
@@ -29,10 +23,20 @@ BEGIN
     SELECT SUM(passing_yds) as passing_yards,
     (SUM(passing_tds) + SUM(rushing_tds) + SUM(receiving_tds) +  SUM(puntret_tds) + SUM(kickret_tds)) as touchdowns,
     (SUM(rushing_yds) + SUM(receiving_yds)) as yards,
-    ptc.player_id
+    ptc.player_id,
+    MAX(ptc.lastProcessedGsisID) as lastProcessedGsisID
     FROM play_player AS pp INNER JOIN playersToCheck as ptc
      ON pp.player_id = ptc.player_id
-     WHERE pp.gsis_id >= ptc.lastProcessedGsisID AND pp.play_id > ptc.lastProcessedPlayID GROUP BY ptc.player_id;
+     WHERE pp.gsis_id >= ptc.lastProcessedGsisID AND pp.play_id > ptc.lastProcessedPlayID AND
+     (pp.passing_tds >= 1 OR pp.rushing_tds >= 1 OR pp.receiving_tds >= 1 OR pp.puntret_tds >= 1 OR pp.kickret_tds >= 1)
+      GROUP BY ptc.player_id;
+
+    -- Get whole game stats
+    UPDATE notifToSend
+    SET passing_yards = (SELECT SUM(passing_yds) FROM play_player as pp WHERE notifToSend.lastProcessedGsisID = pp.gsis_id AND pp.player_id = notifToSend.player_id),
+        touchdowns = (SELECT SUM(passing_tds) + SUM(rushing_tds) + SUM(receiving_tds) +  SUM(puntret_tds) + SUM(kickret_tds) FROM play_player as pp WHERE notifToSend.lastProcessedGsisID = pp.gsis_id AND pp.player_id = notifToSend.player_id),
+        yards = (SELECT SUM(rushing_yds) + SUM(receiving_yds) FROM play_player as pp WHERE notifToSend.lastProcessedGsisID = pp.gsis_id AND pp.player_id = notifToSend.player_id);
+
 
     INSERT INTO notificationToSend
     (messengerSenderIDsToSend,
@@ -50,7 +54,7 @@ BEGIN
     ON notifToSend.player_id = ps.player_id
     INNER JOIN messengerUser AS m
     ON ps.userID = m.userID
-    WHERE notifToSend.touchdowns > 1;
+    WHERE notifToSend.touchdowns >= 1;
 
     UPDATE playersToCheck as ptc
     SET lastProcessedGsisID = (SELECT MAX(gsis_id) FROM play_player as pp where ptc.player_id=pp.player_id),
